a35ff8e1765a1feb04676242dcfb6b86d41772f7
hexgallo <142266031+hexgallo@users.noreply.github.com>
2023-08-15 23:23:40 -0600

    Update streamlit_app.py
    cc4

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,13 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗🐔 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
